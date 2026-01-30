c03beb0576da3c321561ca98580fe9714a38e8f9
Marcin Kulik <m@ku1ik.com>
2026-01-30 10:47:54 +0100

    clean up - remove --nocolor and --nowait options

--- tondeuse.py
+++ tondeuse.py
@@ -8,14 +8,12 @@ def usage():
     print('Usage: ./tondeuse.py [options]')
     print('  -s, --slow=DELAY    slow delay')
     print('  -f, --fast=DELAY    fast delay')
-    print('      --nocolor       self explanatory')
     print('      --miss=RATE     miss per myriad')
-    print('      --nowait        do not wait at the end')
     print('  -i, --nointerrupt   Ctrl+C is not your friend anymore')
     
 try:
     opts, args = getopt.getopt(sys.argv[1:], 's:f:ih',
-                               ['slow=', 'fast=', 'nocolor', 'miss=','nowait',
+                               ['slow=', 'fast=', 'miss=',
                                 'nointerrupt'])
 except getopt.GetoptError as err:
     # print help information and exit:
@@ -26,9 +24,7 @@ except getopt.GetoptError as err:
 slow_delay = 0.5 # time in seconds to mow a ; in slow mode (default mode)
 fast_delay = 0.05 # time in seconds to mow a ; in fast mode
 
-colors = True
 miss_permyriad = 0 # be a perfect mower by default
-wait_at_the_end = True # shall we wait for a keypress once the lawn is mown?
 catch_sigint = False # catch sigint so that no one can stop the mowing?
 
 for o,a in opts:
@@ -39,12 +35,8 @@ for o,a in opts:
     elif o in ('-h'):
         usage()
         sys.exit(0)
-    elif o in ('--nocolor'):
-        colors = False
     elif o in ('--miss='):
         miss_permyriad = float(a)
-    elif o in ('--nowait'):
-        wait_at_the_end = False
     elif o in ('-i','--nointerrupt'):
         catch_sigint = True
         
@@ -55,14 +47,12 @@ import signal
 import time
 
 class lawn:
-    def __init__(self,win,slow_delay,fast_delay,colors,miss_permyriad,wait):
+    def __init__(self,win,slow_delay,fast_delay,miss_permyriad):
         self.term = win
         self.slow_delay = slow_delay
         self.fast_delay = fast_delay
         self.delay = slow_delay
-        self.colors = colors
         self.miss_permyriad = miss_permyriad
-        self.wait_at_the_end = wait
 
         self.grass = ';'
         self.cut_grass = ','
@@ -73,17 +63,11 @@ class lawn:
         curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
 
         self.normal_attr = curses.color_pair(1)
-        if colors:
-            # Grass is green; cut grass is a dimmer green to show it's mowed.
-            self.grass_attr = curses.color_pair(2)
-            self.cut_grass_attr = curses.color_pair(2) | curses.A_DIM
-            # Mower motor is red and bold to stand out.
-            self.motor_attr = curses.color_pair(3) | curses.A_BOLD
-        else:
-            # No-color mode: everything uses the normal attribute.
-            self.grass_attr = self.normal_attr
-            self.cut_grass_attr = self.normal_attr
-            self.motor_attr = self.normal_attr
+        # Grass is green; cut grass is a dimmer green to show it's mowed.
+        self.grass_attr = curses.color_pair(2)
+        self.cut_grass_attr = curses.color_pair(2) | curses.A_DIM
+        # Mower motor is red and bold to stand out.
+        self.motor_attr = curses.color_pair(3) | curses.A_BOLD
 
         (self.garden_h, self.garden_w) = self.term.getmaxyx()
         self.mower_size = 4
@@ -142,9 +126,6 @@ class lawn:
         if c == -1:
             return
 
-        if c == curses.KEY_RESIZE:
-            self.earthquake()
-        
         if c == ord(' '):
             if self.finished:
                 # little hack to allow to quit on space after mowing the
@@ -205,68 +186,6 @@ class lawn:
             if self.y >= self.garden_h:
                 self.finished = True
         
-        # We're done mowing.
-        if self.wait_at_the_end:
-            # wait for a keypress when we're done: we don't want the user to
-            # miss on her beautifully mown lawn!
-            # sleep a bit in-between not to hog the cpu
-            while not self.handle_events():
-                curses.doupdate() # needed to trigger resize events
-                time.sleep(.04)
-
-    def earthquake(self):
-        '''The terminal has been resized! Take action.'''
-
-        # DEBUGME: uncomment below
-        # sys.stderr.write('resize\n')
-        (old_h, old_w) = (self.garden_h, self.garden_w)
-        (old_x, old_y) = (self.x, self.y)
-        old_scr = self.scr
-        
-        (self.garden_h, self.garden_w) = self.term.getmaxyx()
-        if self.y >= self.garden_h or self.finished:
-            self.finished = True
-            self.y = self.garden_h
-        if self.y % 2 == 0:
-            self.x = min(old_x, self.garden_w + 2*self.mower_size +1)
-        else:
-            self.x = min(old_x, self.garden_w + self.mower_size)
-
-        # get a new curses pad to the right dimensions
-        self.init_lawn()
-
-        # fill the new pad with the relevant portion of the old one
-        self.scr.bkgd(ord(self.grass), self.grass_attr)
-        old_scr.overwrite(self.scr)
-
-        # re-mow up to the current position
-        # be careful not to re-draw mowed lawn that is already on screen,
-        # especially since the new positions of mismowed blades of grass will
-        # probably not match those of the old ones.
-        for y in range(self.y):
-            for x in range(old_w + 1, self.garden_w + 1):
-                self.mow_grass(y, x + self.mower_size)
-
-        if self.finished:
-            # draw the rest of the screen as freshly cut grass
-            for y in range(old_y, self.y):
-                for x in range(self.garden_w + 1):
-                    self.mow_grass(y, x + self.mower_size)
-        else:
-            # complete the current row with freshly cut grass if the mower
-            # was on its way to the left of the screen
-            if self.y % 2 == 1:
-                for x in range(old_x, self.garden_w + 1):
-                    self.mow_grass(self.y, x + self.mower_size)
-            
-            # draw the mower in case its position has changed
-            if self.y % 2 == 0:
-                self.right_mower(self.y, self.x)
-            else:
-                self.left_mower(self.y, self.x)
-
-        self.refresh_screen()
-        
     def refresh_screen(self):
         (h, w) = self.term.getmaxyx()
         # warning: race condition! h & w might change before we call refresh.
@@ -279,7 +198,7 @@ def start(win):
     if catch_sigint:
         signal.signal(signal.SIGINT, signal.SIG_IGN)
     
-    l = lawn(win,slow_delay,fast_delay,colors,miss_permyriad,wait_at_the_end)
+    l = lawn(win,slow_delay,fast_delay,miss_permyriad)
     l.mow()
 
 if __name__ == '__main__':
